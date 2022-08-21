d3a51e9be4822b6edc806b9250ad8f876727895b
Chaewon Kim <irene4563@naver.com>
2022-08-22 00:45:43 +0900

    feat: enum

--- typescript/타입스크립트 입문 - 기초부터 실전까지/learn-typescript-master/class-note/5_operator.ts
+++ typescript/타입스크립트 입문 - 기초부터 실전까지/learn-typescript-master/class-note/5_operator.ts
@@ -5,33 +5,33 @@
 // logMessage(10);
 // logMessage(false);
 
-var seho: string | number | boolean;
-function logMessage(value: string | number) {
-    if(typeof value === 'number') {
-        value.toLocaleString();
-    }
-    if(typeof value === 'string') {
-        value.toString();
-    }
-    // 타입 가드: 특정 타입으로 타입의 범위를 좁혀나가는(필터링 하는) 과정
+// var seho: string | number | boolean;
+// function logMessage(value: string | number) {
+//     if(typeof value === 'number') {
+//         value.toLocaleString();
+//     }
+//     if(typeof value === 'string') {
+//         value.toString();
+//     }
+//     // 타입 가드: 특정 타입으로 타입의 범위를 좁혀나가는(필터링 하는) 과정
 
-    throw new TypeError('value must be string or number');
-}
-logMessage('hello')
-logMessage(100);
-// 유니온 타입: 하나 이상의 타입 사용을 하고 싶을 때 사용
+//     throw new TypeError('value must be string or number');
+// }
+// logMessage('hello')
+// logMessage(100);
+// // 유니온 타입: 하나 이상의 타입 사용을 하고 싶을 때 사용
 
 
 
-interface Developer {
-    name: string;
-    skill: string;
-}
+// interface Developer {
+//     name: string;
+//     skill: string;
+// }
 
-interface Person {
-    name: string;
-    age: number;
-}
+// interface Person {
+//     name: string;
+//     age: number;
+// }
 
 // function askSomeone(someone: Developer | Person) {
 // //     someone.name
@@ -43,12 +43,12 @@ interface Person {
 // askSomeone({ name: '디벨로퍼', skill: '웹 개발' });
 // askSomeone({ name: '캡틴', age: 100 });
 
-function askSomeone(someone: Developer & Person) {
-    someone.name
-    someone.skill
-    someone.age
-}
-askSomeone({ name: '디벨로퍼', skill: '웹 개발', age: 34 });
+// function askSomeone(someone: Developer & Person) {
+//     someone.name
+//     someone.skill
+//     someone.age
+// }
+// askSomeone({ name: '디벨로퍼', skill: '웹 개발', age: 34 });
 
 // Developer의 name, skill + Person의 name, age => 총 name, skill, age 3개의 속성 가짐
 // Developer과 Person의 모든 속성을 가짐
---
+++ typescript/타입스크립트 입문 - 기초부터 실전까지/learn-typescript-master/class-note/6_enum.ts
@@ -0,0 +1,39 @@
+enum Shoes {
+    Nike = '나이키',
+    Adidas = '아디다스'
+}
+
+var myShoes = Shoes.Nike;
+console.log(myShoes); // 나이키
+
+// 값을 설정하지 않으면 숫자형 이넘으로 반환함
+
+
+// 예제
+enum Answer {
+    Yes = 'Y',
+    No = 'N'
+}
+
+function askQuestion(answer: string) {
+    if (answer === 'yes') {
+        console.log('정답입니다');
+    }
+    if (answer === 'no') {
+        console.log('오답입니다');
+    }
+}
+askQuestion('예스');
+askQuestion('y');
+askQuestion('Yes');
+
+function askQuestion(answer: Answer) {
+    if (answer === Answer.Yes) {
+        console.log('정답입니다');
+    }
+    if (answer === Answer.No) {
+        console.log('오답입니다');
+    }
+}
+askQuestion(Answer.Yes);
+askQuestion('Yes'); // 에러
