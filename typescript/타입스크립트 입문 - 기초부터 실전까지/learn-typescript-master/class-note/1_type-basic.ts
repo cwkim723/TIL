2434ecb1272c3cac3fa43484bcbfb70ebf00479b
Chaewon Kim <irene4563@naver.com>
2022-08-21 15:27:26 +0900

    feat: 기본 타입

---
+++ typescript/타입스크립트 입문 - 기초부터 실전까지/learn-typescript-master/class-note/1_type-basic.ts
@@ -0,0 +1,30 @@
+// JS 문자열 선언
+// var str = 'hello';
+
+// TS 문자열 선언
+let str: string = 'hello';
+
+let num: number = 10;
+let arr: Array<number> = [1,2,3];
+let heroes: Array<string> = ['Capt', 'Thor', 'Hulk', 10];
+let items: number[] = [1,2,3];
+
+// TS 튜플
+let address: [string, number] = ['gangnam', 10];
+// 타입 위치까지 지정 가능. 배열의 특정 인덱스의 타입을 정하는 것을 튜플이라고 함
+// 튜플: 모든 인덱스의 타입 정의
+
+// TS 객체
+let obj: object = {};
+// let person: object = {
+//     name: 'capt',
+//     age: 100
+// };
+// object이기 때문에 객체의 형식만 갖춰져있으면 타입은 상관 X
+let person: {name: string, age: number} = {
+    name: 'thor',
+    age: 1000
+}
+
+// TS 진위값
+let show: boolean = true; // show라는 변수의 타입은 진위
