317abcfaa03a8d69cda9557ff1d093addb41ae34
Chaewon Kim <irene4563@naver.com>
2022-08-22 00:32:15 +0900

    feat: 유니온 타입과 인터섹션 타입

--- typescript/타입스크립트 입문 - 기초부터 실전까지/learn-typescript-master/class-note/4_type-aliases.ts
+++ typescript/타입스크립트 입문 - 기초부터 실전까지/learn-typescript-master/class-note/4_type-aliases.ts
@@ -3,21 +3,21 @@
 //     age: number;
 // }
 
-type Person = {
-    name: string;
-    age: number;
-}
+// type Person = {
+//     name: string;
+//     age: number;
+// }
 
-var seho: Person = {
-    name: '세호',
-    age: 30
-}
+// var seho: Person = {
+//     name: '세호',
+//     age: 30
+// }
 
 
-type MyString = string;
-var str: MyString = 'hello';
+// type MyString = string;
+// var str: MyString = 'hello';
 
-type Todo = { id: string; title: string; done: boolean };
-function getTodo(todo: Todo) {
+// type Todo = { id: string; title: string; done: boolean };
+// function getTodo(todo: Todo) {
 
-}
+// }
