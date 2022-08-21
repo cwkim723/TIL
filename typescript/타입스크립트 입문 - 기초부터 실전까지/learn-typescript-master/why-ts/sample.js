9c2f9a64cdd4166e959a492cd06830e527c3748c
Chaewon Kim <irene4563@naver.com>
2022-08-21 14:56:29 +0900

    feat: tsc

--- typescript/타입스크립트 입문 - 기초부터 실전까지/learn-typescript-master/why-ts/sample.js
+++ typescript/타입스크립트 입문 - 기초부터 실전까지/learn-typescript-master/why-ts/sample.js
@@ -10,13 +10,13 @@
 
 // @ts-check
 
-/**
- * 
- * @param {number} a 첫 번째 숫자
- * @param {number} b 두 번째 숫자
- * @returns
- */
-function sum(a, b) {
-    return a + b;
-}
-sum(10, '20');
+// /**
+//  * 
+//  * @param {number} a 첫 번째 숫자
+//  * @param {number} b 두 번째 숫자
+//  * @returns
+//  */
+// function sum(a, b) {
+//     return a + b;
+// }
+// sum(10, '20');
