00dddf242fc1d0b628fb17d0a88f12e6fac4e7a4
Chaewon Kim <irene4563@naver.com>
2022-08-22 01:21:17 +0900

    feat: 클래스

---
+++ typescript/타입스크립트 입문 - 기초부터 실전까지/learn-typescript-master/class-note/7_class.ts
@@ -0,0 +1,27 @@
+// class Person {
+//     private name: string; // private: 변수의 접근범위 제한
+//     public age: number;
+//     readonly log: string; // readonly: 읽기만 가능
+
+//     constructor(name: string, age: number) {
+//         this.name = name;
+//         this.age = age;
+//     }
+// }
+
+// // 리액트 예전 문법
+// class App extends React.Component {
+
+// }
+
+// function App() {
+//     return <div>Hello World</div>
+// }
+
+// // 
+// new VTTCue({
+//     el: '',
+//     setup() {
+
+//     }
+// })
---
+++ typescript/타입스크립트 입문 - 기초부터 실전까지/learn-typescript-master/class-note/class.js
@@ -0,0 +1,22 @@
+// // ES2015 (ES6)
+
+// function Person(name, age) {
+//     this.name = name;
+//     this.age = age;
+// }
+// var capt = new Person('캡틴', 100);
+
+// // == 위와 아래의 코드는 완전히 동일 ==
+
+// class Person {
+//     // 클래스 로직
+//     // 클래스 목적: 인스턴스 생성
+//     constructor(name, age) {
+//         console.log('생성 되었습니다.');
+//         this.name = name;
+//         this.age = age;
+//     }
+// }
+
+// var seho = new Person('세호', 30); // 생성 되었습니다.
+// // seho라는 변수에 name = '세호', age = 30 이 담김
