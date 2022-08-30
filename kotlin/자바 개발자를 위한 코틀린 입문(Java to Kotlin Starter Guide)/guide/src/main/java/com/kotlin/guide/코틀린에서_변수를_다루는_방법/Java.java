251a609a626b7b9f68828b3baf6fe9f3997dcb59
Chaewon Kim <irene4563@naver.com>
2022-08-31 00:04:17 +0900

    feat: kotlin, vue

---
+++ kotlin/자바 개발자를 위한 코틀린 입문(Java to Kotlin Starter Guide)/guide/src/main/java/com/kotlin/guide/코틀린에서_변수를_다루는_방법/Java.java
@@ -0,0 +1,20 @@
+package com.kotlin.guide.코틀린에서_변수를_다루는_방법;
+
+public class Java {
+    long number1 = 10L; // (1)
+    final long number2 = 10L; // (2)
+
+    Long number3 = 1_000L; // (3)
+    Person person = new Person("채원"); // (4)
+
+    private class Person {
+        private String name;
+
+        public Person(String name) {
+        }
+    }
+
+
+}
+
+
---
+++ kotlin/자바 개발자를 위한 코틀린 입문(Java to Kotlin Starter Guide)/guide/src/main/java/com/kotlin/guide/코틀린에서_변수를_다루는_방법/Kotlin.kt
@@ -0,0 +1,9 @@
+package com.kotlin.guide.코틀린에서_변수를_다루는_방법
+
+class Kotlin {
+
+    var number1 = 10L
+
+    var number3: Long? = 1_000L
+
+}
