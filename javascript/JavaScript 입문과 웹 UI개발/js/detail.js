30f4ae2466b2ee62af3ebcbf4f6fa335fdb55b4e
Chaewon Kim <irene4563@naver.com>
2022-10-07 17:41:28 +0900

    feat: 탭기능 만들며 배우는 for 반복문 2

--- javascript/JavaScript 입문과 웹 UI개발/js/detail.js
+++ javascript/JavaScript 입문과 웹 UI개발/js/detail.js
@@ -1,36 +1,37 @@
-document.querySelectorAll('.tab-button')[0].addEventListener('click', function() {
+const tabButton = document.querySelectorAll('.tab-button');
+const tabContent = document.querySelectorAll('.tab-content')
+
+tabButton[0].addEventListener('click', function() {
+    // 전체에 있는 orange 다 삭제 -> tabButton해당 버튼에 orange + 해당 위치 content도
+    
+    태그삭제하기()
+
+    tabButton[0].classList.add('orange')
+    tabContent[0].classList.add('show')
+})
+tabButton[1].addEventListener('click', function() {
     // 전체에 있는 orange 다 삭제 -> 해당 버튼에 orange + 해당 위치 content도
     
     태그삭제하기()
 
-    document.querySelectorAll('.tab-button')[0].classList.add('orange')
-    document.querySelectorAll('.tab-content')[0].classList.add('show')
+    tabButton[1].classList.add('orange')
+    tabContent[1].classList.add('show')
 })
-document.querySelectorAll('.tab-button')[1].addEventListener('click', function() {
+tabButton[2].addEventListener('click', function() {
     // 전체에 있는 orange 다 삭제 -> 해당 버튼에 orange + 해당 위치 content도
     
     태그삭제하기()
 
-    document.querySelectorAll('.tab-button')[1].classList.add('orange')
-    document.querySelectorAll('.tab-content')[1].classList.add('show')
+    tabButton[2].classList.add('orange')
+    tabContent[2].classList.add('show')
 })
-document.querySelectorAll('.tab-button')[2].addEventListener('click', function() {
-    // 전체에 있는 orange 다 삭제 -> 해당 버튼에 orange + 해당 위치 content도
-    
-    태그삭제하기()
 
-    document.querySelectorAll('.tab-button')[2].classList.add('orange')
-    document.querySelectorAll('.tab-content')[2].classList.add('show')
-})
 
 function 태그삭제하기() {
-    const buttonArr = document.querySelectorAll('.tab-button')
-    const contentArr = document.querySelectorAll('.tab-content')
-
-    for(let i = 0; i < buttonArr.length; i++) {
-        buttonArr[i].classList.remove('orange');
+    for(let i = 0; i < tabButton.length; i++) {
+        tabButton[i].classList.remove('orange');
     }
-    for(let i = 0; i < contentArr.length; i++) {
-        contentArr[i].classList.remove('show');
+    for(let i = 0; i < tabContent.length; i++) {
+        tabContent[i].classList.remove('show');
     }
 }
