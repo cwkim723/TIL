be19561fb87d3ab2774e78fedc15603ff397760e
Chaewon Kim <irene4563@naver.com>
2022-10-08 23:08:08 +0900

    feat: 이벤트 버블링 응용과 dataset

--- javascript/JavaScript 입문과 웹 UI개발/js/detail.js
+++ javascript/JavaScript 입문과 웹 UI개발/js/detail.js
@@ -1,37 +1,61 @@
 const tabButton = document.querySelectorAll('.tab-button');
-const tabContent = document.querySelectorAll('.tab-content')
+const tabContent = document.querySelectorAll('.tab-content');
 
-tabButton[0].addEventListener('click', function() {
-    // 전체에 있는 orange 다 삭제 -> tabButton해당 버튼에 orange + 해당 위치 content도
+// tabButton[0].addEventListener('click', function() {
+//     // 전체에 있는 orange 다 삭제 -> tabButton해당 버튼에 orange + 해당 위치 content도
     
-    태그삭제하기()
+//     태그삭제하기()
 
-    tabButton[0].classList.add('orange')
-    tabContent[0].classList.add('show')
-})
-tabButton[1].addEventListener('click', function() {
-    // 전체에 있는 orange 다 삭제 -> 해당 버튼에 orange + 해당 위치 content도
+//     tabButton[0].classList.add('orange')
+//     tabContent[0].classList.add('show')
+// })
+// tabButton[1].addEventListener('click', function() {
+//     // 전체에 있는 orange 다 삭제 -> 해당 버튼에 orange + 해당 위치 content도
     
-    태그삭제하기()
+//     태그삭제하기()
 
-    tabButton[1].classList.add('orange')
-    tabContent[1].classList.add('show')
-})
-tabButton[2].addEventListener('click', function() {
-    // 전체에 있는 orange 다 삭제 -> 해당 버튼에 orange + 해당 위치 content도
+//     tabButton[1].classList.add('orange')
+//     tabContent[1].classList.add('show')
+// })
+// tabButton[2].addEventListener('click', function() {
+//     // 전체에 있는 orange 다 삭제 -> 해당 버튼에 orange + 해당 위치 content도
     
-    태그삭제하기()
+//     태그삭제하기()
 
-    tabButton[2].classList.add('orange')
-    tabContent[2].classList.add('show')
-})
+//     tabButton[2].classList.add('orange')
+//     tabContent[2].classList.add('show')
+// })
 
 
-function 태그삭제하기() {
-    for(let i = 0; i < tabButton.length; i++) {
-        tabButton[i].classList.remove('orange');
+// function 태그삭제하기() {
+//     for(let i = 0; i < tabButton.length; i++) {
+//         tabButton[i].classList.remove('orange');
+//     }
+//     for(let i = 0; i < tabContent.length; i++) {
+//         tabContent[i].classList.remove('show');
+//     }
+// }
+
+
+// for (let i = 0; i < tabButton.length; i++) {
+//     document.querySelectorAll('.tab-button')[i].addEventListener('click', function() {
+//         탭열기(i)
+//     })
+// }
+
+// 이벤트 리스너가 줄어들수록 램 용량이 줄어듦
+document.querySelector('.list').addEventListener('click', function(e) {
+    // 지금 누른게 버튼 0이면 버튼0에 orange, 박스0에 show
+    if(e.target ==  document.querySelectorAll('.tab-button')[0]) {
+
     }
-    for(let i = 0; i < tabContent.length; i++) {
-        tabContent[i].classList.remove('show');
+})
+
+function 탭열기(숫자) {
+    for (let i = 0; i < tabButton.length; i++) {
+        document.querySelectorAll('.tab-button')[i].classList.remove('orange');
+        document.querySelectorAll('.tab-content')[i].classList.remove('show');
     }
+    document.querySelectorAll('.tab-button')[숫자].classList.add('orange');
+    document.querySelectorAll('.tab-content')[숫자].classList.add('show')
 }
