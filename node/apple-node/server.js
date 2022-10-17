04bd3684081e3defb84e13d7cfb4969011f2d307
Chaewon Kim <irene4563@naver.com>
2022-10-17 23:36:08 +0900

    feat: 서버에서 HTML 파일전송해보기 & Nodemon으로 자동화

--- node/apple-node/server.js
+++ node/apple-node/server.js
@@ -7,10 +7,17 @@ app.listen(8080, function () {
 });
 
 // 누군가가 /pet으로 방문하면 pet 관련된 안내문을 띄워주기
-app.get('/pet', function(요청, 응답){
+app
+.get('/pet', function(요청, 응답){
     응답.send('펫 용품 쇼핑 페이지입니다.');
+})
+.get('/beauty', function(요청, 응답){
+    응답.send('뷰티 용품 쇼핑 페이지입니다.');
+})
+.get('/', function(요청, 응답){
+    응답.sendFile(__dirname + '/index.html');
 });
 
-app.get('/beauty', function(요청, 응답){
-    응답.send('뷰티 용품 쇼핑 페이지입니다.');
-});
+// 서버 실행 자동화
+// npm install -g nodemon
+// nodemon server.js
