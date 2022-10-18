199ab74b3c2fde5d677e4f004bb982d8b21d76a0
Chaewon Kim <irene4563@naver.com>
2022-10-18 13:46:25 +0900

    feat: 폼에 입력한 데이터를 서버에 전송하는 법 (POST요청)

--- node/apple-node/server.js
+++ node/apple-node/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser') // post요청으로 서버에 데이터 전송하고 싶으면 body-parser 필요
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.listen(8080, function () {
     // 서버띄울 포트번호, 띄운 후 실행할 코드
@@ -7,8 +9,9 @@ app.listen(8080, function () {
 });
 
 // 누군가가 /pet으로 방문하면 pet 관련된 안내문을 띄워주기
+// 함수 안에 함수 = 콜백함수 -> 순차적으로 실행하고싶을 때 사용
 app
-.get('/pet', function(요청, 응답){
+.get('/pet', (요청, 응답) => {
     응답.send('펫 용품 쇼핑 페이지입니다.');
 })
 .get('/beauty', function(요청, 응답){
@@ -24,3 +27,11 @@ app
 // 서버 실행 자동화
 // npm install -g nodemon
 // nodemon server.js
+
+// post 요청으로 서버에 데이터 전송하고 싶으면 form데이터의 경우 input들에 name 작성하기
+app
+.post('/add', function(요청, 응답) {
+    응답.send('전송완료');
+    console.log(요청.body); // { title: '오늘의 할일', date: '날짜' }
+    console.log(요청.body.title); // 오늘의 할일
+})
